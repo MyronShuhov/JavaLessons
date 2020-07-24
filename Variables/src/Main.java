0f135d9743b3834d0781807a2377c6210af01f29
MyronShyhov <mirosh125@gmail.com>
2020-07-24 17:53:34 +0200

    Reference data types

--- Variables/src/Main.java
+++ Variables/src/Main.java
@@ -1,28 +1,14 @@
 import java.sql.SQLOutput;
 
 public class Main {
-//Целые
-    static byte b = 5;
-    static short sh = 8;
-    static int it = 99;
-    static long l = 758;
-//дробные
-    static float f1 = 2.6f;
-    static double db = 2.6;
 
-    static boolean bool1 = true;
-    static boolean bool2 = false;
+    static int CONST = 55;
 
     public static void main(String[] args){
 
-        System.out.println("byte: " + b);//sout
-        System.out.println("short: " + sh);//sout
-        System.out.println("int: " + it);//sout
-        System.out.println("long: " + l);//sout
-        System.out.println("float: " + f1);//sout
-        System.out.println("double: " + db);//sout
-        System.out.println(bool1);
-        System.out.println(bool2);
+        System.out.println(CONST);//sout
+        CONST = 88;
+        System.out.println(CONST);
 //ctrl+d - копировать строку
 //ctrl+y - удалить строку
     }
